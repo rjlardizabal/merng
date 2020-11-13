69800c14d91fc7872e075496759ba6ada9d10dd7
mynameisarjay <mambaisnegro24>
2020-11-13 19:43:10 +1300

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose.connect(MONGODB, { useNewUrlParser: true }).then(() => {
+  console.log("MONGOOSE connected");
+  return server.listen({ port: 5000 }).then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
+});
