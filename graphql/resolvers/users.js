d2bf5c9e0e88e6c0af5a8c7087d8f79bf363ef18
mynameisarjay <mambaisnegro24>
2020-11-13 21:47:34 +1300

    added user authentication

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,10 +1,25 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { UserInputError } = require("apollo-server");
-const { validateRegisterInput } = require("../../util/validators");
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../util/validators");
 
 const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
+
+generateToken = (user) => {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+};
 
 module.exports = {
   Mutation: {
@@ -19,10 +34,10 @@ module.exports = {
         password,
         confirmPassword
       );
-      if(!valid) {
-        throw new UserInputError('Errors', {
-          errors
-        })
+      if (!valid) {
+        throw new UserInputError("Errors", {
+          errors,
+        });
       }
       // TODO make sure user doesnt already exist
       const user = await User.findOne({ username });
@@ -45,19 +60,37 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        SECRET_KEY,
-        { expiresIn: "1h" }
-      );
+      const token = generateToken(res);
 
       return {
         ...res._doc,
         id: res._id,
+        token,
+      };
+    },
+
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+      const user = await User.findOne({ username });
+      if (!user) {
+        errors.general = "User not found!";
+        throw new UserInputError("User not found", { errors });
+      }
+      if (!valid) {
+        throw new UserInputError("Errors", errors);
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong credentials";
+        throw new UserInputError("Wrong credentials", { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
         token,
       };
     },
