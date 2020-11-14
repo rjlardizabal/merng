734b0fb3dc6ae8c26ee88ec223bf9a3ed543d511
mynameisarjay <mambaisnegro24>
2020-11-15 00:12:08 +1300

    added routing and semantic ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import MenuBar from "./components/MenuBar";
 function App() {
   return (
-    <div>
-      <h1>Hi world!</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
